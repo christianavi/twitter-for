5d6f0d23cd94635bdb3e9f6338c1ff3e33f93554
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2020-06-04 18:36:32 +0800

    Update twitter-for.py

--- twitter-for.py
+++ twitter-for.py
@@ -1,9 +1,14 @@
 import tweepy
 from credentials import *
+
 auth = tweepy.OAuthHandler(apikey, apisecretkey) 
 auth.set_access_token(accesstoken, accesstokensecret) 
+
 api = tweepy.API(auth)
-print ("Twitter for [Version 1.0.0]")
+
+print ("twitter-for [Version 1.0.1]")
 print ("Copyright (c) 2020 christianavi. All rights reserved.")
+
 tweet = input("What's happening? ")
+
 api.update_status(status =(tweet))
