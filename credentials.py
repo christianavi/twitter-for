d56762ae23522ebf7018babdb87b8a9e63b35304
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2020-06-04 16:44:53 +0800

    Uh, who are you?
    Added a separate credentials file to avoid accidental modification of the main file.

---
+++ credentials.py
@@ -0,0 +1,4 @@
+apikey = "api-key"
+apisecretkey = "api-secret-key"
+accesstoken = "access-token"
+accesstokensecret = "access-token-secret"
--- twitter-for.py
+++ twitter-for.py
@@ -1,9 +1,9 @@
 import tweepy
-auth = tweepy.OAuthHandler("api-key", "api-secret-key") 
-auth.set_access_token("access-token", "access-token-secret") 
+from credentials import *
+auth = tweepy.OAuthHandler(apikey, apisecretkey) 
+auth.set_access_token(accesstoken, accesstokensecret) 
 api = tweepy.API(auth)
 print ("Twitter for [Version 1.0.0]")
-print ("Copyright (c) 2020 Warm Bananas. All rights reserved.")
+print ("Copyright (c) 2020 christianavi. All rights reserved.")
 tweet = input("What's happening? ")
 api.update_status(status =(tweet))
-
